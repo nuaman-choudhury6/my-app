1a5346bc60695294c796d3b9a867d3be89ceaa0a
nuaman-choudhury6 <nuamanchy66@gmail.com>
2024-02-19 12:02:51 +0530

    task 8. advanced react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ExpenseItem from './components/ExpenseItem.js';
+import React, { useState } from 'react';
 
-function App() {
-  const expenses = [
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+  const [expenses, setExpenses] = useState([
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -22,32 +24,19 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ];
-  return (
-    <div className="App">
-      <ExpenseItem
-       title={expenses[0].title}
-       amount={expenses[0].amount}
-       date={expenses[0].date}
-       ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[1].title}
-      amount={expenses[1].amount}
-      date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-       title={expenses[2].title}
-       amount={expenses[2].amount}
-       date={expenses[2].date}
-       ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[3].title}
-       amount={expenses[3].amount}
-       date={expenses[3].date}
-       ></ExpenseItem>
+  ]);
 
+  const addExpenseHandler = (expense) => {
+    // Update state with the new expense
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
